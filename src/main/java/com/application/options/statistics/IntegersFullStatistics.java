bda8db17328063914c611f91a96de8278245ac3b
Xasthurs <1zxxe@mail.ru>
2024-07-25 22:09:15 +0700

    ...

--- src/main/java/com/application/options/statistics/IntegersFullStatistics.java
+++ src/main/java/com/application/options/statistics/IntegersFullStatistics.java
@@ -6,10 +6,17 @@ public class IntegersFullStatistics implements StatisticsProvider{
     private final StatisticsManagerForNumbers statManagerForNumbers = new StatisticsManagerForNumbers();
     @Override
     public void provide(String pathToFile) {
-        int min = (int) statManagerForNumbers.findMinNumber(pathToFile);
-        int max = (int) statManagerForNumbers.findMaxNumber(pathToFile);
-        int average = (int) statManagerForNumbers.findAverageNumber(pathToFile);
-        int sum = (int) statManagerForNumbers.findSumNumber(pathToFile);
+        float minFloat = (float) statManagerForNumbers.findMinNumber(pathToFile);
+        float maxFloat = (float) statManagerForNumbers.findMaxNumber(pathToFile);
+        float averageFloat = (float) statManagerForNumbers.findAverageNumber(pathToFile);
+        float sumFloat = (float) statManagerForNumbers.findSumNumber(pathToFile);
+
+        int min = (int) minFloat;
+        int max = (int) maxFloat;
+        int average = (int) averageFloat;
+        int sum = (int) sumFloat;
+
         statisticsPrinter.printFullStat(min, max, average, sum, "integer");
     }
+
 }
